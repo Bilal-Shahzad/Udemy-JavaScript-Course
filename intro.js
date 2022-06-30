2d294246f8f4ed51f4a0bbbc45fe3dcfb920017f
Bilal-Shahzad <b.shahzad01@gmail.com>
2022-06-30 16:08:39 -0500

    Section 2, content 1-7. Learning about strings and variables

---
+++ intro.js
@@ -0,0 +1,11 @@
+var pet1 = "Bear"     
+var baseballTeam = "Dodgers"
+var favoriteDayOfTheWeek = "Friday"
+var favorite_color = "green"
+var travel = "car"
+
+// possible solutions
+
+var pet1 = 'Bear'
+var baseballTeam = 'Dodgers'
+ 
---
+++ string-lab.js
@@ -0,0 +1,28 @@
+// var animal = 'horse"
+// var answer = false;
+
+// solution 
+var animal = "horse";
+var answer = 'false';
+
+// challenge 2 
+// var greeting 1 = "Sam says, "history. ";
+// var greeting 2 = 'It's great to see you!';
+
+// solution to rewrite to make valid strings 
+
+var greeting1 = "Sam says, 'hi.";
+var greeting2 = "It's great to see you!";
+
+
+// challenge 2 
+// var greeting 3 = Don\'t forget to grab the newspaper\n on your way out 
+// You are going to be\n on the bus for a long time because of traffic. '
+
+// var greeting3 = "Don't forget to grab the newspaper on your way out. 
+// You are going to be on the bus for a long time because of traffic."
+
+greeting3.length;
+120
+greeting3.toUpperCase();
+"DON'T FORGET TO GRAB THE NEWSPAPER ON YOUR WAY OUT. YOU ARE GOING TO BE ON THE BUS FOR A LONG TIME BECAUSE OF THE TRAFFIC."
