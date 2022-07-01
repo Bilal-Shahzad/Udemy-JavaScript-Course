8c8a8827964e936160f7538b4374f923b2ec74b1
Bilal-Shahzad <b.shahzad01@gmail.com>
2022-07-01 13:44:21 -0500

    the rest of section 2 complete, chapters 8-12. Strings, operators, increment adn decrement operators

---
+++ operators-string-example.js
@@ -0,0 +1,8 @@
+var firstName = 'Rob'
+var middleName = 'Grant'
+var lastName = 'Merrill'
+
+var fullName = firstName + '' + middleName + '' + lastName;
+// dont forget to add a string with a space place holder otherwise the name will be all cramped up 
+
+fullName; 'Rob Grant Merrill'
