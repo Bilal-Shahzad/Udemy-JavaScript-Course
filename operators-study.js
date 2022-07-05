cdf375db5c6be4239e2f517468682c2535aa20fd
Bilal-Shahzad <b.shahzad01@gmail.com>
2022-07-05 13:06:31 -0500

    fixing notes adding definitions for each word and clarifying examples

--- operators-study.js
+++ operators-study.js
@@ -1,32 +1,47 @@
 // binary operators 
+// Requires two operands, one before the operator and one after the operator.
+// operands- the quantity on which an operation is to be done.
 
 // 2 + 3; or x * z
 // 4 + 5; or x * y
 
 // unary operators
+// Requires a single operand, either before or after the operato
 
 // ++, +, *, - , --, &
 
 // Arithmetic Operators: Multiplication, Division, Modulus, Addition and Subtraction
+// Takes numerical values as their operands and returns a single number
 
 var count = 10 + 5 - 4; // 11
 
 var mult = 3 * 5; // 15
 
 //Modulus Operators
-
+// This will give us the remainder of two values.
 
 21 % 4; // 1
 
 
 //Assignment Operators
+// Assigns a value to its left operand based on the value of its right operand.
+// operand- the quantity on which an operation is to be done.
+// basically the "="
 
 var count = 5;
 
-//Comparison Operators:
+//Comparison Operators
+// Compare both sides of equation and returns a  value based on if the comparison is true. They can be numerical, string, logical, or object values.
 
-
+// Checks for equality in value. (Equality operator)
 5 == '5'; // true
 
 5 === '5' // false
+// === Checks for equality in value and type. (Strict equality operator)
 
+
+// sting operator
+// When the ‘+’ is used on strings the ‘+’ operator is called the concatenation operator.
+// the + is the string operator 
+
+// 'Hello' + ' ' + 'world'; // 'Hello world' 
