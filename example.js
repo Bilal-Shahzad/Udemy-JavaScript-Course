6a1e2039ffa25126d7e13b37a7c892c37f70aac1
Bilal-Shahzad <b.shahzad01@gmail.com>
2022-07-07 13:47:31 -0500

    arrays and strings practice

---
+++ example.js
@@ -0,0 +1,36 @@
+var myArray = ['1,2,3,4'];    
+
+var firstValue = myArray[0];      
+
+var secondValue = myArray[1];      
+
+console.log(firstValue); //1
+console.log(secondValue); //2
+
+
+
+// ex 2
+
+var untensils = ['spoon', 'fork', 'knife'];
+//push adds an element 
+arr.push('napkin')
+
+arr.push('plate','bowl')
+
+console.log(utentils)
+//'spoon', 'fork', 'knife', 'napkin'
+console.log(untensils)
+// spoon', 'fork', 'knife', 'napkin', 'plate', 'bowl'
+
+
+
+//ex3
+
+var colors = ['wallet', 'blue', 'white', 'green'];
+
+//shift removes the first element from the array 
+colors.shift()
+//'wallet'
+
+console.log(colors)
+//'blue', 'white', 'green'
