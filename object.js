d561191a8036d006c5ad5bc9848a2926d50d5a31
Bilal-Shahzad <b.shahzad01@gmail.com>
2022-07-08 13:22:01 -0500

    going over bracket notation and dot notation with objects.

---
+++ object.js
@@ -0,0 +1,40 @@
+// bracket notation is accessing the value with brackets 
+var car = {
+    wheels: 4,
+    color: 'red',
+    mpg: 34,
+    engine: 1,
+    gallons: 12
+}
+
+
+//ex2 
+// three property values that have string, number and boolean 
+//boolean- system that creates statements 
+var humanBeing = {
+    hungry: true,
+    age: 20,
+    height : '5\'5'
+};
+
+Object {hungry: true, age: 20, height "5'5"}
+
+
+// object with literal notation
+// dot notation with 4 properties and values 
+
+// pizza -object, crust -property, wheat - value
+pizza.crust = 'wheat';
+"wheat"
+
+pizza.sauce = 'marinara';
+"marinara"
+
+pizza.cheese = 'mozzarella';
+"mozzarella"
+
+pizza.toppings = 'cheese, mushrooms, pineapple';
+"cheese, pepporoni, olives, mushroom "
+
+pizza
+Object {crust: "wheat", sauce: "marinara" . cheese : "mozzarella", toppings : "cheese, mushrroms, pineapple" }
