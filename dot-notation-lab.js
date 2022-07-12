ad0f73aa2e34ecf6fdb4f16b6bfee065e8872cf0
Bilal-Shahzad <b.shahzad01@gmail.com>
2022-07-12 13:11:55 -0500

    dot notation lab questions and solutions

---
+++ dot-notation-lab.js
@@ -0,0 +1,43 @@
+//Q1
+
+var snowman = {
+    isSnow: true, 
+    color: white,
+    madeOfSnowballs: 3,
+    'first name': 'Frosty'
+};
+
+
+//access all the properties of object 
+
+snowman.isSnow
+//true
+
+snowman.color
+"white"
+
+snowman.madeOfSnowballs 
+3
+
+snowman;
+Object {isSnow: "yes", color "orange", madeOfSnowballs : 3, firstName: Frosty
+
+
+//challenge 2
+
+house {};
+Object {}
+
+house.doors = 4;
+4
+house.bedroooms = 3;
+3
+
+house.adress = '1917 s Orthello street';
+"1917 s Orthello street"
+
+house.office = false;
+false
+
+house 
+Object { doors: 4, bedrooms 3, adress: "1917 s Orthello Street", office: false}
