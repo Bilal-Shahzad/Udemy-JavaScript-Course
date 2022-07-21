c7a23308dad9a2eb06c03a8641da0f63466ec655
Bilal-Shahzad <b.shahzad01@gmail.com>
2022-07-21 12:53:11 -0500

    array lab, and the different type of array methods

---
+++ array-lab.js
@@ -0,0 +1,24 @@
+//practie 
+
+var baseballTeams = ['Dodgers', 'Giants', 'Mets', 'Yankees', 'Astros']
+
+//possible solutioins 
+baseballTeams[6] = 'Cardinals';
+"Cardinals"
+baseballTeams[6] = 'Blue Jays';
+"Blue Jays "
+console.log=vbaseballTeams
+["Dodgers","Yankees","Mets","Astros","Cardinals","Blue Jays"]
+
+
+baseballTeams
+["Dodgers","Yankees","Mets","Astros","Cardinals","Blue Jays"]
+
+
+baseballTeams[1]= 'Angels';
+"Angels"
+baseballTeams[4]= 'Phillies';
+"Phillies"
+
+console.log 
+["Angels","Yankees","Mets","Phillies","Cardinals","Blue Jays"]
